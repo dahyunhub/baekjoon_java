13a50445779648ed9d37dfe859fde08983519054
dahyunhub <tls848@naver.com>
2025-08-01 23:27:57 +0900

    Solve: BOJ 1001 A-B, BOJ 10998 AxB

--- src/bronze/No2884.java
+++ src/bronze/No2884.java
@@ -11,21 +11,22 @@ public class No2884 {
 
 		int newH = 0;
 		int newM = 0;
-		
+
 		if (H >= 0 && H <= 23 && M >= 0 && M <= 59) {
 			newM = M - 45;
-					
+
 			if (newM < 0) {
 				newH = H - 1;
 				newM = 60 + newM;
 				if (newH < 0) {
 					newH = 24 + newH;
 				}
-			}  else {
+			} else {
 				newH = H;
 			}
-			
+
 			System.out.print(newH + " " + newM);
+			sc.close();
 		}
 	}
 }

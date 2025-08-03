63b8e05811065ace28b0916603e63ec7de59d915
dahyunhub <tls848@naver.com>
2025-08-03 19:13:29 +0900

    Solve: BOJ 10869 사칙연산

---
+++ src/bronze/No10869.java
@@ -0,0 +1,21 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No10869 {
+	public static void main(String [] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A >= 1 && B <= 10000) {
+			System.out.println(A + B);
+			System.out.println(A - B);
+			System.out.println(A * B);
+			System.out.println(A / B);
+			System.out.println(A % B);
+		}
+
+		sc.close();
+	}
+}

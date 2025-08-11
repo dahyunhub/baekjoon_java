8d11fec2e92e045935c88ca0360369ace45b83d8
dahyunhub <tls848@naver.com>
2025-08-11 21:58:18 +0900

    Solve: BOJ 1330 두 수 비교하기

---
+++ src/bronze/No1330.java
@@ -0,0 +1,23 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No1330 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if (A >= -10000 && A <= 10000 || B >= -10000 && B <= 10000) {
+			if (A > B) {
+				System.out.println(">");
+			} else if (A < B) {
+				System.out.println("<");
+			} else if (A == B) {
+				System.out.println("==");
+			}
+		}
+		sc.close();
+	}
+}

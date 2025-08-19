6cfb9824d8792abecf4233299eb2449e17632e70
dahyunhub <tls848@naver.com>
2025-08-19 22:04:41 +0900

    Update: BOJ 11021 A+B-7 - modify import

--- src/bronze/No11021.java
+++ src/bronze/No11021.java
@@ -1,16 +1,13 @@
 package bronze;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 
 public class No11021 {
 	public static void main(String[] args) {
 
 		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			 BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		) {
 			int t = Integer.parseInt(br.readLine());
 			int a, b;

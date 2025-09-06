6f19e2ca9c0accc65d5e0134b8b5d288a055da64
dahyunhub <tls848@naver.com>
2025-09-06 21:41:37 +0900

    Solve: BOJ 10818 최소, 최대 (Scanner version)

---
+++ src/bronze/No10818.java
@@ -0,0 +1,28 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No10818 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; ++i) {
+			arr[i] = sc.nextInt();
+		}
+
+		int min = arr[0];
+		int max = arr[0];
+
+		for (int i = 0; i < arr.length; ++i) {
+			min = arr[i] < min ? arr[i] : min;
+			max = arr[i] > max ? arr[i] : max;
+		}
+
+		System.out.println(min + " " + max);
+		sc.close();
+	}
+}

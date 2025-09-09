2a04c371af0bc2b6703d4b8f49743e54735a346d
dahyunhub <tls848@naver.com>
2025-09-09 21:17:50 +0900

    Solve: BOJ 1550 16진수

---
+++ src/bronze/No1550.java
@@ -0,0 +1,15 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No1550 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+			String str = sc.nextLine();
+			int result = Integer.parseInt(str, 16);
+
+			System.out.println(result);
+			sc.close();
+	}
+}

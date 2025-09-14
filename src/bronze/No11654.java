eef6e091c450ebdcbacd7756bf2e8f415b3bb0cd
dahyunhub <tls848@naver.com>
2025-09-14 11:41:03 +0900

    Solve: BOJ 11654 아스키 코드 - System.in.read(): 입력한 문자의 byte 값을 그대로 int로 반환(한 글자만 입력할 때 적합함)

---
+++ src/bronze/No11654.java
@@ -0,0 +1,19 @@
+package bronze;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class No11654 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int ch;
+		
+		try {
+			ch = System.in.read();
+			System.out.println(ch);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}

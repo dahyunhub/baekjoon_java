b5df0fcec85ee6c68f6e81375da407a09e3b0202
dahyunhub <tls848@naver.com>
2025-09-21 16:33:35 +0900

    Solve: BOJ 10871 X보다 작은 수

---
+++ src/bronze/No10871.java
@@ -0,0 +1,23 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No10871 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int x = sc.nextInt();
+
+		int[] a = new int[n];
+
+		for (int i = 0; i < a.length; ++i) {
+			a[i] = sc.nextInt();
+
+			if (a[i] < x) {
+				System.out.print(a[i] + " ");
+			}
+		}
+		sc.close();
+	}
+}

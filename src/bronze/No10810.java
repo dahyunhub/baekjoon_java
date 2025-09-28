0f4e1791e0029773199fccaeb7e41d706408b85e
dahyunhub <tls848@naver.com>
2025-09-28 19:16:18 +0900

    Solve: BOJ 10810 공 넣기

--- src/bronze/No10810.java
+++ src/bronze/No10810.java
@@ -5,25 +5,27 @@ import java.util.Scanner;
 public class No10810 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int m = sc.nextInt();
-		
+
 		int[] arr = new int[n];
-		
-		for (int num = 1; num <= m; ++num) {
+
+		for (int num = 0; num < m; ++num) {
 			int i = sc.nextInt();
 			int j = sc.nextInt();
 			int k = sc.nextInt();
 
-			int index = i;
-			
-			while (index <= j) {
+			int index = i - 1;
+
+			while (index < j) {
 				arr[index] = k;
 				++index;
 			}
-			System.out.println(arr[i]);
 		}
-		
+
+		for (int i = 0; i < n; ++i) {
+			System.out.print(arr[i] + " ");
+		}
 	}
 }

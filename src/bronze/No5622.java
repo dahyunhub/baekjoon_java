d9714fab0d130945896d97206ce3fa171e0e51bc
dahyunhub <tls848@naver.com>
2025-11-03 23:01:55 +0900

    Solve: BOJ 5622 다이얼

---
+++ src/bronze/No5622.java
@@ -0,0 +1,25 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No5622 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String word = sc.next();
+		String dial[] = { "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS", "TUV", "WXYZ" };
+
+		int n = 0;
+		int totalTime = 0;
+
+		for (int i = 0; i < word.length(); ++i) {
+			for (int j = 0; j < dial.length; ++j) {
+				if (dial[j].contains(String.valueOf(word.charAt(i)))) {
+					n = (j + 2) + 1;
+					totalTime += n;
+				}
+			}
+		}
+		System.out.println(totalTime);
+		sc.close();
+	}
+}

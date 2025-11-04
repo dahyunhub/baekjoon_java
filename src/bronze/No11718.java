c21d02aa6ddcc7adad549020aae8759f6438aa94
dahyunhub <tls848@naver.com>
2025-11-04 22:07:20 +0900

    Solve: BOJ 11718 그대로 출력하기 (입력의 끝(EOF) 처리)

---
+++ src/bronze/No11718.java
@@ -0,0 +1,13 @@
+package bronze;
+
+import java.util.Scanner;
+
+public class No11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while (sc.hasNextLine()) {
+			System.out.println(sc.nextLine());
+		}
+	}
+}

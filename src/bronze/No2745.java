36aad2b8240d7e40e78406935ff03fb8e5834751
dahyunhub <tls848@naver.com>
2025-12-26 23:30:40 +0900

    Solve: BOJ 2745 진법 변환

---
+++ src/bronze/No2745.java
@@ -0,0 +1,18 @@
+package bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No2745 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] input = br.readLine().split(" ");
+
+		String n = input[0];
+		int b = Integer.parseInt(input[1]);
+
+		System.out.println(Integer.parseInt(n, b));
+	}
+}

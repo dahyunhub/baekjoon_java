9a38b94d32b4ae9d9745f1ed8d1a38780096376b
dahyunhub <tls848@naver.com>
2026-03-01 20:31:56 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 19548 KB -SsafyToday

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		Deque<Integer> deque = new ArrayDeque<>();
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			deque.offerLast(i);
+		}
+
+		sb.append("<");
+		int i = 1;
+		while (!deque.isEmpty()) {
+			if (i % K == 0) {
+				sb.append(deque.poll());
+				if (!deque.isEmpty()) {
+					sb.append(", ");
+				}
+			} else {
+				int next = deque.poll();
+				deque.offer(next);
+			}
+			++i;
+		}
+		sb.append(">");
+		System.out.println(sb.toString());
+	}
+}

d917a0660a005a7adda16935db4c91a4fabf08bc
Dahyun Shin <tls848@naver.com>
2026-06-12 13:43:25 +0900

    [Passed] 계단 오르기 (126ms, 10MB)

---
+++ Codetree/trail4/계단 오르기/climbing-stairs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int[] dp = new int[n+1];
+
+        if (n >= 2) dp[2] = 1;
+        if (n >= 3) dp[3] = 1;
+
+        for (int i = 4; i <=n ; i++) {
+            dp[i] = (dp[i-3] + dp[i-2]) % 10007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
